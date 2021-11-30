be16f0bb3cbe1c96ec4c03985f8920077753e5aa
Hadrien Croubois <hadrien.croubois@gmail.com>
2021-11-30 10:51:12 +0100

    working on some deployment scripts

---
+++ scripts/MigrationManager.js
@@ -0,0 +1,70 @@
+const { ethers } = require('ethers');
+const prompts    = require('prompts');
+const AsyncConf  = require("./AsyncConf");
+
+function confirm(message = 'Confirm') {
+    return prompts({ type: 'confirm', name: 'confirm', message }).then(({ confirm }) => confirm);
+}
+
+class MigrationManager {
+    constructor(provider, config = {}) {
+        this.provider = provider;
+        this.config   = config;
+
+        this.cacheAsPromise = provider.getNetwork().then(({ name, chainId }) => {
+            this.cache = new AsyncConf({ configName: `.cache-${chainId}` });
+            return this.cache;
+        });
+    }
+
+    ready() {
+        return Promise.all([
+            this.cacheAsPromise,
+        ])
+        .then(() => this);
+    }
+
+    migrate(key, factory, args = [], opts = {}) {
+        return this.ready()
+        // confirm deployment (clean cache if needed)
+        .then(() => opts.override
+            ? this.cache.delete(key) || this.cache.delete(`${key}-pending`) || true
+            : this.cache.get(key).then(value => !!value || confirm(`Deploy "${key}" with params:\n${JSON.stringify(args, null, 4)}\nConfirm`))
+        )
+        // fetchOrDeploy
+        .then(deploy => deploy
+            ? this.resumeOrDeploy(key, () => opts.kind
+                ? upgrades.deployProxy(factory, args, opts)
+                : factory.deploy(...args)
+            )
+            : undefined
+        )
+        // attach to address
+        .then(address => address
+            ? factory.attach(address)
+            : undefined
+        );
+    }
+
+    async resumeOrDeploy(key, deploy) {
+        let txHash  = await this.cache.get(`${key}-pending`);
+        let address = await this.cache.get(key);
+
+        if (!txHash && !address) {
+            const contract = await deploy();
+            txHash = contract.deployTransaction.hash;
+            await this.cache.set(`${key}-pending`, txHash);
+            await contract.deployed();
+            address = contract.address;
+            await this.cache.set(key, address);
+        } else if (!address) {
+            address = await this.provider.getTransaction(txHash)
+            .then(tx => tx.wait())
+            .then(receipt => receipt.contractAddress);
+            await this.cache.set(key, address);
+        }
+        return address;
+    }
+}
+
+module.exports = MigrationManager;
--- scripts/index.js
+++ scripts/index.js
@@ -1,48 +1,9 @@
-const { ethers, upgrades } = require('hardhat');
-
-Array.range = function(start, stop = undefined, step = 1) {
-    if (!stop) { stop = start; start = 0; }
-    return start < stop ? Array(Math.ceil((stop - start) / step)).fill().map((_, i) => start + i * step) : [];
-}
-
-Array.prototype.unique = function(op = x => x) {
-    return this.filter((obj, i) => this.findIndex(entry => op(obj) === op(entry)) === i);
-}
-
-Array.prototype.chunk = function(size) {
-    return Array.range(Math.ceil(this.length / size)).map(i => this.slice(i * size, i * size + size))
-}
-
-Buffer.prototype.chunk = function(size) {
-    return Array.range(Math.ceil(this.length / size)).map(i => this.slice(i * size, i * size + size))
-}
-
-async function getFactory(name, opts = {}) {
-    return ethers.getContractFactory(name).then(contract => contract.connect(opts.signer || contract.signer));
-}
-
-function attach(name, address, opts = {}) {
-    return getFactory(name, opts).then(factory => factory.attach(address));
-}
-
-function deploy(name, args = [], opts = {}) {
-    if (!Array.isArray(args)) { opts = args; args = []; }
-    return getFactory(name, opts).then(factory => factory.deploy(...args)).then(contract => contract.deployed());
-}
-
-function deployUpgradeable(name, kind, args = [], opts = {}) {
-    if (!Array.isArray(args)) { opts = args; args = []; }
-    return getFactory(name, opts).then(factory => upgrades.deployProxy(factory, args, { kind })).then(contract => contract.deployed());
-}
-
-function performUpgrade(proxy, name, opts = {}) {
-    return getFactory(name, opts).then(factory => upgrades.upgradeProxy(proxy.address, factory, {}));
-}
+const AsyncConf        = require("./AsyncConf");
+const MigrationManager = require("./MigrationManager");
+const deploy           = require("./deploy");
 
 module.exports = {
-    getFactory,
-    attach,
-    deploy,
-    deployUpgradeable,
-    performUpgrade,
-};
+    AsyncConf,
+    MigrationManager,
+    ...deploy,
+}
