3973f40c13e48605637aa6b5b61a33c1b7820b96
Hadrien Croubois <hadrien.croubois@gmail.com>
2022-02-09 18:16:24 +0100

    Improve hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,15 +3,20 @@ require('dotenv/config');
 const argv = require('yargs/yargs')(process.argv.slice(2))
   .env('')
   .options({
+    // modules
     coverage:      { type: 'boolean',                                          default: false        },
     report:        { type: 'boolean',                                          default: false        },
-    slow:          { type: 'boolean',                                          default: false        },
-    compiler:      { type: 'string',                                           default: '0.8.10'     },
-    chainId:       { type: 'number',                                           default: 1337         },
+    // compilations
+    compiler:      { type: 'string',                                           default: '0.8.11'     },
     hardfork:      { type: 'string',                                           default: 'london'     },
     mode:          { type: 'string', choices: [ 'production', 'development' ], default: 'production' },
     runs:          { type: 'number',                                           default: 200          },
+    revertStrings: { type: 'string', choices: [ 'default', 'strip'          ], default: 'default'    },
+    // chain
     fork:          { type: 'string',                                                                 },
+    chainId:       { type: 'number',                                           default: 1337         },
+    slow:          { type: 'boolean',                                          default: false        },
+    // APIs
     coinmarketcap: { type: 'string'                                                                  },
     etherscan:     { type: 'string'                                                                  },
   })
@@ -20,8 +25,8 @@ const argv = require('yargs/yargs')(process.argv.slice(2))
 require('@nomiclabs/hardhat-waffle');
 require('@nomiclabs/hardhat-ethers');
 require('@openzeppelin/hardhat-upgrades');
-require('solidity-coverage');
 
+args.coverage  && require('solidity-coverage');
 argv.etherscan && require('@nomiclabs/hardhat-etherscan');
 argv.report    && require('hardhat-gas-reporter');
 
@@ -30,13 +35,16 @@ const settings = {
     enabled: argv.mode === 'production' || argv.report,
     runs: argv.runs,
   },
+  debug: {
+    revertStrings: argv.revertStrings,
+  },
 };
 
 module.exports = {
   solidity: {
     compilers: [
       { version: argv.compiler, settings },
-      { version: '0.8.10',      settings },
+      { version: '0.8.11',      settings },
       { version: '0.7.6',       settings },
       { version: '0.6.12',      settings },
       { version: '0.5.16',      settings },
@@ -65,11 +73,26 @@ const accounts = [
 Object.assign(
   module.exports.networks,
   accounts && Object.fromEntries([
-    'mainnet',
-    'ropsten',
-    'rinkeby',
-    'goerli',
-    'kovan',
+    // main
+    'mainnet', 'ropsten', 'rinkeby', 'goerli', 'kovan',
+    // binance smart chain
+    'bsc', 'bscTestnet',
+    // huobi eco chain
+    'heco', 'hecoTestnet',
+    // fantom mainnet
+    'opera', 'ftmTestnet',
+    // optimism
+    'optimisticEthereum', 'optimisticKovan',
+    // polygon
+    'polygon', 'polygonMumbai',
+    // arbitrum
+    'arbitrumOne', 'arbitrumTestnet',
+    // avalanche
+    'avalanche', 'avalancheFujiTestnet',
+    // moonbeam
+    'moonbeam', 'moonriver', 'moonbaseAlpha',
+    // xdai
+    'xdai', 'sokol',
   ].map(name => [ name, { url: argv[`${name}Node`], accounts } ]).filter(([, { url} ]) => url)),
   argv.slow && { hardhat: { mining: { auto: false, interval: [3000, 6000] }}}, // Simulate a slow chain locally
   argv.fork && { hardhat: { forking: { url: argv.fork }}}, // Simulate a mainnet fork
