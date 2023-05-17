ee88dd4f4947d06e32cf23e78fde4c2d477dd77f
Hadrien Croubois <hadrien.croubois@gmail.com>
2023-05-17 18:36:39 +0200

    add opt to deployUpgradeable

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -15,7 +15,7 @@ function deploy(name, args = [], opts = {}) {
 
 function deployUpgradeable(name, kind, args = [], opts = {}) {
     if (!Array.isArray(args)) { opts = args; args = []; }
-    return getFactory(name, opts).then(factory => upgrades.deployProxy(factory, args, { kind })).then(contract => contract.deployed());
+    return getFactory(name, opts).then(factory => upgrades.deployProxy(factory, args, { kind, ...opts })).then(contract => contract.deployed());
 }
 
 function performUpgrade(proxy, name, opts = {}) {
